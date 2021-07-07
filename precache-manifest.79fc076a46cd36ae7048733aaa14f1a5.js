aa41a666f4d57fcd9861d967010f31bb69a70c64
Karan Kumar k <karankabinesh@gmail.com>
2021-07-07 12:39:24 +0530

    Updates

---
+++ precache-manifest.79fc076a46cd36ae7048733aaa14f1a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db16c2ada82ce53de7711ddbff67d36c",
+    "url": "/RR_Reactjs/index.html"
+  },
+  {
+    "revision": "91a423e8ceba54a674ff",
+    "url": "/RR_Reactjs/static/js/2.7fcbdb1e.chunk.js"
+  },
+  {
+    "revision": "00e2b2cdc2475b76a408969f1af93df0",
+    "url": "/RR_Reactjs/static/js/2.7fcbdb1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8864789fc24857e1f8",
+    "url": "/RR_Reactjs/static/js/main.128c6286.chunk.js"
+  },
+  {
+    "revision": "7372f988f2938b5c81b4",
+    "url": "/RR_Reactjs/static/js/runtime-main.f833bf6e.js"
+  }
+]);
