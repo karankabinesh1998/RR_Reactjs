8fd4f00a0689f7712e1c5ac9a029a7f58c567a11
Karan Kumar k <karankabinesh@gmail.com>
2021-07-07 13:09:12 +0530

    Updates

---
+++ precache-manifest.a386bcf874cf085eb194119aa2a1bd42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c75aafbb2df2956aebf007333b9ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a423e8ceba54a674ff",
+    "url": "/static/js/2.7fcbdb1e.chunk.js"
+  },
+  {
+    "revision": "00e2b2cdc2475b76a408969f1af93df0",
+    "url": "/static/js/2.7fcbdb1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7067c349700b49b491c8",
+    "url": "/static/js/main.5f3ebaaf.chunk.js"
+  },
+  {
+    "revision": "7308dd93756735fe9a6a",
+    "url": "/static/js/runtime-main.ef316f09.js"
+  }
+]);
